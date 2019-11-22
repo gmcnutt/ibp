0cb021c828648d51452d436f443babe642f669f1
gmcnutt <gmcnutt@cableone.net>
2019-11-22 11:58:56 -0700

    Simplified pixel format output

--- demo.c
+++ demo.c
@@ -5,102 +5,7 @@
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
-/*
-static const char *SDL_PIXELFORMAT_STRINGS[] = {
-        "UNKNOWN",
-        "INDEX1LSB",
-        "INDEX1MSB",
-        "INDEX4LSB",
-        "INDEX4MSB",
-        "INDEX8",
-        "RGB332",
-        "RGB444",
-        "RGB555",
-        "BGR555",
-        "ARGB4444",
-        "RGBA4444",
-        "ABGR4444",
-        "BGRA4444",
-        "ARGB1555",
-        "RGBA5551",
-        "ABGR1555",
-        "BGRA5551",
-        "RGB565",
-        "BGR565",
-        "RGB24",
-        "BGR24",
-        "RGB888",
-        "RGBX8888",
-        "BGR888",
-        "BGRX8888",
-        "ARGB8888",
-        "RGBA8888",
-        "ABGR8888",
-        "BGRA8888",
-        "ARGB2101010",
-        "RGBA32",
-        "ARGB32",
-        "BGRA32",
-        "ABGR32",
-        "YV12",
-        "IYUV",
-        "YUY2",
-        "UYVY",
-        "YVYU",
-        "NV12",
-        "NV21"
-};
-*/
-static const char *SDL_PIXELTYPE_STRINGS[] = {
-        "UNKNOWN",
-        "INDEX1",
-        "INDEX4",
-        "INDEX8",
-        "PACKED8",
-        "PACKED16",
-        "PACKED32",
-        "ARRAYU8",
-        "ARRAYU16",
-        "ARRAYU32",
-        "ARRAYF16",
-        "ARRAYF32",
-};
 
-static const char *SDL_BITMAPORDER_STRINGS[] = {"NONE", "4321", "1234"};
-
-static const char *SDL_PACKEDORDER_STRINGS[] = {
-        "NONE",
-        "XRGB",
-        "RGBX",
-        "ARGB",
-        "RGBA",
-        "XBGR",
-        "BGRX",
-        "ABGR",
-        "BGRA"
-};
-
-static const char *SDL_ARRAYORDER_STRINGS[] = {
-        "NONE",
-        "RGB",
-        "RGBA",
-        "ARGB",
-        "BGR",
-        "BGRA",
-        "ABGR"
-};
-
-static const char *SDL_PACKEDLAYOUT_STRINGS[] = {
-        "NONE",
-        "332",
-        "4444",
-        "1555",
-        "5551",
-        "565",
-        "8888",
-        "2101010",
-        "1010102"
-};
 struct args {
         char **filenames;
         int filecount;
@@ -194,7 +99,6 @@ void sdl_dump_pixel_format(const SDL_PixelFormat *format)
 static void dump_imageinfo(struct imageinfo *info)
 {
         SDL_PixelFormat *format = info->image->format;
-        const char *type, *order, *layout;
 
         /* Basic info */
         printf(
@@ -205,31 +109,7 @@ static void dump_imageinfo(struct imageinfo *info)
                 );
 
         /* Pixel format info */
-        type = SDL_PIXELTYPE_STRINGS[SDL_PIXELTYPE(format->format)];
-        order = "---";
-        layout = "---";
-
-        if (SDL_ISPIXELFORMAT_INDEXED(format->format)) {
-                order = SDL_BITMAPORDER_STRINGS[SDL_PIXELORDER(format->format)];
-        } else if (SDL_ISPIXELFORMAT_PACKED(format->format)) {
-                order = SDL_PACKEDORDER_STRINGS[SDL_PIXELORDER(format->format)];
-                layout = SDL_PACKEDLAYOUT_STRINGS[SDL_PIXELLAYOUT(format->format)];
-        } else if (SDL_ISPIXELFORMAT_ARRAY(format->format)) {
-                order = SDL_ARRAYORDER_STRINGS[SDL_PIXELORDER(format->format)];
-                layout = SDL_PACKEDLAYOUT_STRINGS[SDL_PIXELLAYOUT(format->format)];
-        };
-
-        printf(" %10s %10s %10s", type, order, layout);
-        printf(" %5d %5d", format->BitsPerPixel, format->BytesPerPixel);
-        printf(" %5s", SDL_ISPIXELFORMAT_ALPHA(format->format) ? "y" : "n");
-        printf(
-                 " %08x %08x %08x %08x",
-                 format->Rmask,
-                 format->Gmask,
-                 format->Bmask,
-                 format->Amask
-                );
-        printf(" %10s", SDL_GetPixelFormatName(format->format));
+        printf(" %28s", SDL_GetPixelFormatName(format->format));
 
         
         /* File info */
@@ -285,21 +165,11 @@ int main(int argc, char **argv)
         
         /* Print header */
         printf(
-                "%5s %5s %5s %10s %10s %10s %5s %5s %3s %8s %8s %8s %8s %10s %s\n",
+                "%5s %5s %5s %28s %s\n",
                 "w",
                 "h",
                 "pitch",
-                "pxtype",
-                "pxord",
-                "pxlay",
-                "pxbit",
-                "pxbyt",
-                "alpha",
-                "rmask",
-                "gmask",
-                "bmask",
-                "amask",
-                "format",
+                "pixfmt",
                 "name"
                 );
 
